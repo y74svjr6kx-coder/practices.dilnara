5cad0b33ec07a7a10a38710ac238f50bb128b86d
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:07:22 +0500

    Create boolean_comparison.py

---
+++ Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,14 @@
+age = 18
+print(age >= 18)
+print(age < 18)
+
+x = 15
+y = 20
+print(x > y)
+print(x < y)
+print(x == y)
+
+temperature = 25
+print(temperature > 20)
+print(temperature == 25)
+print(temperature < 10)
