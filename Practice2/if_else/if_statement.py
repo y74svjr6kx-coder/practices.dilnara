ffb0e5c35fe2d217ba3d26a4724497e91e76dabd
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:25:36 +0500

    Update if_statement.py

--- Practice2/if_else/if_statement.py
+++ Practice2/if_else/if_statement.py
@@ -1,1 +1,13 @@
+number = 5
+if number > 0:
+  print("The number is positive")
 
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
