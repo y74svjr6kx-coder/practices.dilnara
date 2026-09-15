246c143ab44389eda2235d4998db25b3834f5418
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 20:40:41 +0500

    Create casting.py

---
+++ Practice1/python-basics/casting.py
@@ -0,0 +1,39 @@
+x = float(8)
+y = float(9.8)
+z = float("5")
+w = float("2.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+x = int(5)
+y = int(6.8)
+z = int("2")
+print(x)
+print(y)
+print(z)
+
+x = str("pp2")
+y = str(2)
+z = str(5.0)
+print(x)
+print(y)
+print(z)
+
+x = 1
+a = float(x)
+b = str(x)
+print(a)
+print(b)
+
+
+x = 7
+y = float(x)
+z = str(y)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
