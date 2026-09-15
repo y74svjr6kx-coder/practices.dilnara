35f3f93636b527937d44cbf855efeaa32c24a37d
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 20:23:30 +0500

    Create numbers.py

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,27 @@
+x = 10
+y = 5.5
+z = 2 + 3j
+print(type(x))
+print(type(y))
+print(type(z))
+
+price = 12.5
+quantity = 4
+ttl = price * quantity
+print("Total:", ttl)
+
+import random
+number = random.randrange(1, 11)
+print("Random number:", number)
+
+price = 15.5
+quantity = 3
+total = price * quantity
+print("Price:", price)
+print("Quantity:", quantity)
+print("Total:", total)
+
+age = 18
+future_age = age + 5
+print("My age:", age)
+print("In 5 years:", future_age)
