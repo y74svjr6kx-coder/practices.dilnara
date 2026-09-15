96e81433a45026ed7c8a04039411520cc87f4696
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:43:06 +0500

    Create short_hand_if.py

---
+++ Practice2/if_else/short_hand_if.py
@@ -0,0 +1,18 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+a = 5
+b = 303
+print("A") if a > b else print("B")
+
+a = 5
+b = 30
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+x = 7
+y = 30
+max_value = x if x > y else y
+print("Maximum value:", max_value)
+
