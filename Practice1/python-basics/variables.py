ac9e064338b261a4173728cb7e4e34d76185a779
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 19:44:11 +0500

    Update variables.py

--- Practice1/python-basics/variables.py
+++ Practice1/python-basics/variables.py
@@ -1,1 +1,28 @@
+a = 7
+b = "Dilnara"
+print(type(a))
+print(type(b))
 
+x, y, z = "Java", "C++", "Python"
+print(x)
+print(y)
+print(z)
+x = "awesome"
+
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+
+x = 90
+y = 76
+print(x + y)
+
+fruits = ["apple", "banana", "orange"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
