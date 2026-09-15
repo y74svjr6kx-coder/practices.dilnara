7b0e71f83ac6f39e8cf4749d8e78999f219e86aa
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-16 00:01:02 +0500

    Create for_continue.py

---
+++ Practice2/loops/for_continue.py
@@ -0,0 +1,16 @@
+colors = [“red”, “green”, “blue”, “yellow”]
+for color in colors:
+　　if color == “green”:
+　　　　continue
+　　print(color)
+
+for x in range(1, 11):
+　　if x % 2 == 0:
+　　　　continue
+　　print(x)
+
+fruits = [“apple”, “banana”, “cherry”]
+for fruit in fruits:
+　　if fruit == “banana”:
+　　　　continue
+　　print(fruit)
