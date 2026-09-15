a2c751db5cfcb3438b1e67add77a9f61d1dc6dc9
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 20:02:47 +0500

    Create data_types.py

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,35 @@
+x = 9
+y = 3.67
+z = "hi"
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+name = "Dilnara"
+age = 18
+gpa = 3.1
+is_student = True
+print(name, type(name))
+print(age, type(age))
+print(gpa, type(gpa))
+print(is_student, type(is_student))
+
+
+countries = ["France", "Italy", "Spain"]
+visited = {"France", "Kazakhstan"}
+print(countries, type(countries))
+print(visited, type(visited))
+
+
+age = "18"
+print(age, type(age))
+age = int(age)
+print(age, type(age))
+next_year = age + 1
+print("Next year I will be", next_year)
+
+
+numbers = range(1, 10)
+print("Numbers:", list(numbers))
+print("Data type:", type(numbers))
