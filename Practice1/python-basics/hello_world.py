efd194690b489cc7153470a333dd2e59b85de3a0
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 19:30:25 +0500

    Update hello_world.py

--- Practice1/python-basics/hello_world.py
+++ Practice1/python-basics/hello_world.py
@@ -1,1 +1,11 @@
+print("Hello, World!")
 
+name = "Dilnara"
+age = 18 
+language = "python"
+
+print("I am", age, "years old.")
+print ("My name is", name)
+print ("Next year I will be", age+1)
+
+print ("Currently I'm studying", language)
