06304fae919dd98b17567840c1f19a0cab44bb85
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:59:25 +0500

    Create while_continue.py

---
+++ Practice2/loops/while_continue.py
@@ -0,0 +1,28 @@
+i = 0
+while i < 5:
+　　i += 1
+　　if i == 3:
+　　　　continue
+　　print(i)
+
+i = 0
+while i < 10:
+　　i += 1
+　　if i == 5:
+　　　　continue
+　　print(i)
+
+number = 0
+while number < 6:
+　　number += 1
+　　if number == 2:
+　　　　continue
+　　print(“Number:”, number)
+
+
+age = 15
+while age < 20:
+　　age += 1
+　　if age == 18:
+　　　　continue
+　　print(“Age:”, age)
