99900f041cbe131e278c71f0c00e56a0baae23cf
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:41:09 +0500

    Create if_elif_else.py

---
+++ Practice2/if_else/if_elif_else.py
@@ -0,0 +1,19 @@
+a = 200
+b = 55
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+temperature = 22
+if temperature > 30:
+  print("It's hot outside!")
+elif temperature > 20:
+  print("It's warm outside")
+elif temperature > 10:
+  print("It's cool outside")
+else:
+  print("It's cold outside!")
+
