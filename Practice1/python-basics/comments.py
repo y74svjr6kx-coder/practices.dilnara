6c8ff3a8f68fa7dfd1186d8a36e4a4576effc531
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 21:40:54 +0500

    Create comments.py

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,14 @@
+#This is a comment
+print("Hello, World!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just 1 line
+"""
+print("Hello, World!")
