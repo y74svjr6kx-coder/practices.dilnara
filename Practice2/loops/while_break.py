73e0624cd7cdf680eee4712137ced423022eb2d8
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:57:35 +0500

    Create while_break.py

---
+++ Practice2/loops/while_break.py
@@ -0,0 +1,28 @@
+i = 1
+while i < 10:
+    print(i)
+    if i == 5:
+        break
+    i += 1
+
+
+number = 10
+while number > 0:
+    print(number)
+    if number == 7:
+        break
+    number -= 1
+
+x = 2
+while x <= 20:
+    print(x)
+    if x == 10:
+        break
+    x += 2
+
+i = 1
+while True:
+    print("Round:", i)
+    if i == 3:
+        break
+    i += 1
