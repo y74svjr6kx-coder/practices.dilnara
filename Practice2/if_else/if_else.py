23a28a539a578f29830854281bbdb9dd5ddb645e
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:30:56 +0500

    Create if_else.py

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,43 @@
+a = 22
+b = 22
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+score = 94
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+day = 5
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
+
+
+age = 25
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+elif age >= 65:
+  print("You are a senior")
