eaf5b5da6009439b17ec5a4b629c0aea055d5194
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-16 00:00:01 +0500

    Create for_loop.py

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,14 @@
+fruits = [“apple”, “banana”, “cherry”]
+for fruit in fruits:
+　　print(fruit)
+
+
+for x in range(5):
+　　print(x)
+
+for x in range(1, 6):
+　　print(x)
+
+name = “Python”
+for letter in name:
+　　print(letter)
