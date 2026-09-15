e4a57e3ec98353a342fd3db1b1a661f73ff6b39c
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 23:00:59 +0500

    Create boolean_intro.py

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,16 @@
+name = "Dilnara"
+age = 18
+print(bool(name))
+print(bool(age))
+
+
+fruits = ["apple", "banana"]
+empty_list = []
+print(bool(fruits))
+print(bool(empty_list))
+
+
+
+print(bool(""))
+print(bool(0))
+print(bool(None))
