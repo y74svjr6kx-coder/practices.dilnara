8080bf4d67746cd269499ca8400caed889370e4e
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-15 21:08:59 +0500

    Create string.py

---
+++ Practice1/python-basics/string.py
@@ -0,0 +1,17 @@
+a = "Hello, World!"
+print(a[1])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = " Hello, World! "
+print(a.strip()) 
+
+price = 30
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
