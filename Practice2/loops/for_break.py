f64fb44323730425ffee383d2bd91331e221c334
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-16 00:00:34 +0500

    Create for_break.py

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,21 @@
+or x in range(1, 10):
+　　print(x)
+　　if x == 5:
+　　　　break
+
+colors = [“red”, “green”, “blue”, “yellow”]
+for color in colors:
+　　if color == “blue”:
+　　　　break
+　　print(color)
+
+for x in range(10, 20):
+　　print(x)
+　　if x == 15:
+　　　　break
+
+names = [“Anna”, “Dilnara”, “John”, “Alex”]
+for name in names:
+　　print(name)
+　　if name == “John”:
+　　　　break
